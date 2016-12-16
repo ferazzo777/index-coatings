9892c04bdc71bd9cc71e50e5598702c55d0e76e4
Ryan <hey@mix-tape.io>
2016-12-16 20:24:27 +0800

    Amend header and footer

--- wp-content/themes/index-coatings/templates/layout/header.php
+++ wp-content/themes/index-coatings/templates/layout/header.php
@@ -3,7 +3,7 @@
     <a class="header-logo" href="<?php echo home_url(); ?>/"><img src="<?php image_directory() ?>/logo-color.svg" alt=""><span class="hide"><?php bloginfo('name'); ?></span></a>
 
 
-    <a href="/quote" class="header-cta button button--green">Get a <strong>quote</strong></a>
+    <a href="<?php echo get_permalink( get_page_by_path( 'get-a-quote' ) ); ?>" class="header-cta button button--green">Get a <strong>quote</strong></a>
 
     <i class="icon icon-menu header-menu-toggle js-primary-navigation-toggle"></i>
 

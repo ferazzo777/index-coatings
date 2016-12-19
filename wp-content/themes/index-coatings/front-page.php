2e4dd07d2cd0a5f32dd3b6eca794ba7232cedfb4
Ryan <hey@mix-tape.io>
2016-12-19 15:56:36 +0800

    Add feature list

--- wp-content/themes/index-coatings/front-page.php
+++ wp-content/themes/index-coatings/front-page.php
@@ -1,3 +1,4 @@
 <?php get_template_part('templates/content/page'); ?>
+<?php get_template_part('templates/modules/feature-list'); ?>
 <?php get_template_part('templates/modules/testimonials'); ?>
 <?php get_template_part('templates/modules/gallery-slider'); ?>

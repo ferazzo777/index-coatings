4ef52e7e5587a22279efd2df450ccb3f95dd80ae
Mr Mark <mark@birdbrain.com.au>
2017-01-12 14:39:47 +0800

    Remove slider gallery on home

--- wp-content/themes/index-coatings/front-page.php
+++ wp-content/themes/index-coatings/front-page.php
@@ -1,5 +1,4 @@
 <?php get_template_part('templates/content/home'); ?>
-<?php get_template_part('templates/content/page'); ?>
 <?php get_template_part('templates/modules/feature-list'); ?>
 <?php get_template_part('templates/modules/testimonials'); ?>
 <?php get_template_part('templates/modules/gallery-slider'); ?>

4a2cf2289bd4dbfecd8c0b0ea518b69e088c1956
Mr Mark <mark@birdbrain.com.au>
2017-01-19 12:35:28 +0800

    Shortcode fix

--- wp-content/themes/index-coatings/library/tweaks/shortcodes.php
+++ wp-content/themes/index-coatings/library/tweaks/shortcodes.php
@@ -1,9 +1,17 @@
 <?php
 function show_sub_pages() {
   global $post;
-  echo '<ul class="sub-page-list">';
-  wp_list_pages( array('child_of' => $post->ID, 'title_li' => '', 'depth' => '1' ));
-  echo '</ul>';
+
+  if ( is_page() && $post->post_parent )
+  	$childpages = wp_list_pages( 'sort_column=menu_order&title_li=&child_of=' . $post->post_parent . '&echo=0&depth=1' );
+  else
+  	$childpages = wp_list_pages( 'sort_column=menu_order&title_li=&child_of=' . $post->ID . '&echo=0&depth=1' );
+
+  if ( $childpages ) {
+  	$string = '<ul class="sub-page-list">' . $childpages . '</ul>';
+  }
+
+  return $string;
 }
 add_shortcode('sub_pages', 'show_sub_pages');
 ?>

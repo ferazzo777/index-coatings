bd764a4ecf7fbc9d39fec8cf883f44069fa619a0
Mr Mark <mark@birdbrain.com.au>
2017-01-19 12:57:53 +0800

    Remove score bar

--- wp-content/themes/index-coatings/library/tweaks/shortcodes.php
+++ wp-content/themes/index-coatings/library/tweaks/shortcodes.php
@@ -1,14 +1,28 @@
 <?php
+function has_children() {
+    global $post;
+
+    $children = get_pages( array( 'child_of' => $post->ID ) );
+    if( count( $children ) == 0 ) {
+        return false;
+    } else {
+        return true;
+    }
+}
+
 function show_sub_pages() {
   global $post;
-  
-  	$childpages = wp_list_pages( 'sort_column=menu_order&title_li=&child_of=' . $post->ID . '&echo=0&depth=1' );
+  if ( is_page() && has_children() )  	
+    $childpages = wp_list_pages( 'sort_column=menu_order&title_li=&child_of=' . $post->ID . '&echo=0&depth=1' );
+  else
+  	$childpages = wp_list_pages( 'sort_column=menu_order&title_li=&child_of=' . $post->post_parent . '&echo=0&depth=1' );
 
   if ( $childpages ) {
   	$string = '<ul class="sub-page-list">' . $childpages . '</ul>';
+    return $string;
   }
 
-  return $string;
+
 }
 add_shortcode('sub_pages', 'show_sub_pages');
 ?>

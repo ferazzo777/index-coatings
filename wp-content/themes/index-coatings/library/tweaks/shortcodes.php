ab483ab6f10cc9c16499aacd4ae824eb7d882f0f
Mr Mark <mark@birdbrain.com.au>
2017-01-19 12:38:47 +0800

    Shortcode

--- wp-content/themes/index-coatings/library/tweaks/shortcodes.php
+++ wp-content/themes/index-coatings/library/tweaks/shortcodes.php
@@ -1,10 +1,7 @@
 <?php
 function show_sub_pages() {
   global $post;
-
-  if ( is_page() && $post->post_parent )
-  	$childpages = wp_list_pages( 'sort_column=menu_order&title_li=&child_of=' . $post->post_parent . '&echo=0&depth=1' );
-  else
+  
   	$childpages = wp_list_pages( 'sort_column=menu_order&title_li=&child_of=' . $post->ID . '&echo=0&depth=1' );
 
   if ( $childpages ) {
